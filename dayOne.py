85e3cd006e9667ba8d4193b37c8f3ab359d182dd
Gaurav <gaurav.2897@gmail.com>
2019-07-06 01:06:53 +0530

    Added Examples
    Added some more Examples

--- dayOne.py
+++ dayOne.py
@@ -45,6 +45,33 @@ jfjjfjjfkjfkfkkf"""
 
 print(test)
 
+
+# single quotes
+sq_str='Who was "that  person  or new one"?'
+
+print(sq_str)
+# double quotes
+dq_str="Height varies from 5' to 3'."
+
+print(dq_str)
+# escaped quotes
+eq_str= "\"That's pretty good !\" said his personal assistance."
+# creates: "That's not fair!" yelled my sister
+print(eq_str)
+# triple quoted strings, with single quotes
+tq_str= '''This one string can go
+over several lines'''
+print(tq_str)
+# "raw" strings, mostly used for regular expressions
+r_str= r"\"That's not fair!\" yelled my sister."
+# creates: \"That's not fair!\" yelled my sister
+
+print(r_str)
+
+# You can even have raw triple double quoted strings!
+rs_tq_str ="""So there!"""
+
+
 #escaping the \ 
 newline=r"Hey ! How are you  windows path \now i am  here?"
 
